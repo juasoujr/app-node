f4529d93d216f8f1a9180629c7c482322361fa1d
Juarez <juasoujr@gmail.com>
2021-06-01 10:31:13 -0300

    adding last updates

---
+++ angular1/app/config/routes.js
@@ -0,0 +1,13 @@
+angular.module('primeiraApp').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "dashboard/dashboard.html"
+        }).state('billingCycle', {
+            url: "/billingCycles",
+            templateUrl: "billingCycle/tabs.html"
+        })
+    }
+])
