c4e767e7a1f946734b1a45106f102c6a105a6215
Juarez Souza <juarez.souza@viewdeck.com>
2021-06-10 17:03:00 -0300

    adding msgs factories

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,6 +1,7 @@
 (function(){
     angular.module('primeiraApp').controller('BillingCycleCtrl', [
         '$http',
+        'msg',
         BillingCycleController
     ])
 
@@ -11,7 +12,8 @@
             const url = 'http://localhost:3003/api/billingCycles'
             $http.post(url, vm.billingCycle).then(function(response) {
                 vm.refresh()
-                msgs.addSuccess('Operação Realizada com sucesso!!')}).catch(function(response){
+                msgs.addSuccess('Operação Realizada com sucesso!!')
+            }).catch(function(response) {
                     msgs.addError(response.data.errors)
                 })
         }
