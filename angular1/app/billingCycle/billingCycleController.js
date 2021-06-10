9a3be69719f34a30b27ebdcebf6de24674cae960
juarez.souza <juarez.souza@viewdeck.com>
2021-06-10 18:06:13 -0300

    renaming msgsFactory

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -5,17 +5,17 @@
         BillingCycleController
     ])
 
-    function BillingCycleController($http) {
+    function BillingCycleController($http, msgs) {
         const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
 
-        vm.create = function() {
-            const url = 'http://localhost:3003/api/billingCycles'
+        vm.create = function() {  
             $http.post(url, vm.billingCycle).then(function(response) {
                 vm.refresh()
-                msgs.addSuccess('Operação Realizada com sucesso!!')
-            }).catch(function(response) {
-                    msgs.addError(response.data.errors)
-                })
+                msgs.addSuccess('Operação realizada com sucesso!')
+              }).catch(function(response) {
+                msgs.addError(response.data.errors)
+            })
         }
     }
 
